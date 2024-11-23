2ed0bab3166ecb0a3b9a39ee105463bbc0494454
Jose Angel de Bustos Perez <jadebustos@gmail.com>
2024-11-23 19:29:23 +0100

    delete unused staff

--- estructura-practica/ansible/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l XXXX playbook
-
--- estructura-practica/terraform/credentials.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "<SUBSCRIPCION ID>"
-  client_id       = "<APP_ID>"
-  client_secret   = "<PASSWORD>"
-  tenant_id       = "<TENANT>"
-}
